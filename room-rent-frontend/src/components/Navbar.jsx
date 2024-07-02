37d38a478c788fab743415764cc3365a4ca17ef5
Myat Thiha Naing <myatthihanaing.mthn@gmail.com>
2024-07-02 19:28:54 +0800

    created custom dropdown

--- room-rent-frontend/src/components/Navbar.jsx
+++ room-rent-frontend/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { FiAlignJustify } from "react-icons/fi";
 import { IoCloseOutline } from "react-icons/io5";
 
+const languages = ["java", "javascript", "swift", "c#", "ruby", "c", "c++", "assembly", "python" ]
+
 function Navbar(){
     const[isMenuOpen, setMenuOpen] = useState(false)
 
@@ -13,7 +15,7 @@ function Navbar(){
         setMenuOpen(false)
     }
 
-    return <header className="relative w-full bg-slate-600 ">
+    return <header className="relative w-full h-12 bg-slate-600 ">
         <nav>
             <ul className=" flex justify-between p-4">
                 <li>Logo</li>
@@ -22,16 +24,68 @@ function Navbar(){
                 </li>
             </ul>
         </nav>
-        <FilterScreenForMobile isOpen={isMenuOpen}/>
+        <FilterScreenForMobile isOpen={isMenuOpen} key={isMenuOpen}>
+            <SearchBar/>
+            <SearchParameters/>
+        </FilterScreenForMobile>
     </header>
 }
 
-function FilterScreenForMobile({isOpen}){
-    const open = isOpen? " translate-x-0 " : "-translate-x-full"
-    return <div className={`w-full h-dvh absolute top-full right-0
-                              transform duration-500
-                             flex bg-green-600 z-10 ${open} `}>
 
+function FilterScreenForMobile({isOpen,children}){
+    const open = isOpen? " translate-x-0 " : "-translate-x-full"
+    return <div className={`w-full absolute top-full right-0
+                              transform duration-500 flex-col items-center
+                              flex bg-slate-600 z-10 p-4 ${open} `}
+                style={{height: 'calc(100vh - 48px)'}}>
+            
+            {children}
+
+    </div>
+}
+
+function SearchBar(){
+    return <div>
+        <input type="text" placeholder="Search by location" className=" w-56 h-12 p-2 rounded-xl"/>
+    </div>
+}
+
+function SearchParameters(){
+    return <div className=" grid grid-cols-2">
+        <DropDownItem name="Language" options={languages}/>
+        <DropDownItem name="Language" options={languages}/>
+        <DropDownItem name="Language" options={languages}/>
+        <DropDownItem name="Language" options={languages}/>
+    </div>
+}
+
+function DropDownItem({name, options}){
+    const[isOpen, setOpen] = useState(false);
+    const[selectedValue, setSelectedValue] = useState(undefined);
+    
+    const onOptionClicked = (option) => {
+        setSelectedValue(option)
+        setOpen(false)
+    }
+    return <div className=" w-full flex flex-col p-2">
+        <button className=" relative bg-red-300 p-2 w-fit"
+                onClick={()=>{setOpen(!isOpen)}}>
+            {name}:{selectedValue}
+            {isOpen && 
+                <div className=" absolute z-10 top-full left-0 w-full max-h-40 bg-white mt-1 overflow-y-scroll">
+                    <ul>
+                        {options.map(option =>
+                            <li key={option} 
+                                value={option}
+                                className=" border py-1"
+                                onClick={()=>onOptionClicked(option)}>
+                                {option}
+                            </li>)}
+                    </ul>
+                </div>
+            }
+        </button>
+        
     </div>
 }
 
